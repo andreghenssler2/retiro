e0011d4c2e017fa812f2a204ca2720c6c2514b4e
Andre Gustavo Henssler <andrehenssler@sou.faccat.br>
2026-04-09 20:00:31 -0300

    Retiro Teste - vs 0.0.0.0600+beta

--- install/admin/novos/usuario.php
+++ install/admin/novos/usuario.php
@@ -5,23 +5,157 @@ $lang = $_GET['lang'] ?? 'pt_br';
 <div class="card-body">
     <div class='m-2'>
         <div class="col-md-12">
-            
+
         </div>
         <div class="row col-md-12 justify-content-center">
             <div class="col-md-8 ">
-                <form action="#" id='usuario_form' autocomplete="off" accept-charset="utf-8" method="post" class="mform">
+                <form action="#" id='usuario_form' autocomplete="off" accept-charset="utf-8" method="post"
+                    class="mform">
                     <input type="hidden" name="id_form" id="id_form" value="6">
                     <input type="hidden" name="idioma" id="idioma" value="<?php echo $lang ?>">
-                    <!-- <div class="form-group -->
+                    <div class="clearfix">
+                        <div class="col-md-12">
+                            <div class="card-title">
+                                <h5>Nova conta</h5>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_username">
+                                    Login (nome de usuário)
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control " name="username" id="id_username" value=""
+                                    autocomplete="off">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_username"> </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-1"> </div>
+                            <div class="col-md-11"> A senha deve ter ao menos 8 caracteres, ao menos 1 número(s), ao
+                                menos 1 letra(s) minúscula(s), ao menos 1 letra(s) maiúscula(s), no mínimo 1
+                                caractere(s) não alfa-numéricos, como *, -, ou #.
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_username">
+                                    Senha
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control " name="username" id="id_username" value=""
+                                    autocomplete="off">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_username"> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="crearfix">
+                        <div class="col-md-12">
+                            <div class="card-title">
+                                <h5>Informações de Contato</h5>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_email">
+                                    E-mail
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="email" class="form-control " name="email" id="id_email" value=""
+                                    autocomplete="off">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_email"> </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_email_confirmar">
+                                    E-mail Confirmar
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="email" class="form-control " name="email" id="id_email_confirmar" value=""
+                                    autocomplete="off">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_email_confirmar">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="crearfix">
+                        <div class="col-md-12">
+                            <div class="card-title">
+                                <h5>Informações Pessoais</h5>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_nome">
+                                    Nome
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control " name="nome" id="id_nome" value=""
+                                    autocomplete="off">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_nome"> </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col-md-4">
+                                <sapn class="float-sm-right text-nowrap">
+                                    <abbr class="initialism text-danger" title="Necessários">
+                                        <i class="icon fa fa-exclamation-circle text-danger fa-fw " title="Necessários"
+                                            aria-label="Necessários"></i>
+                                    </abbr>
+                                </sapn>
+                                <label class="col-form-label d-inline " for="id_cpf">
+                                    CPF
+                                </label>
+                            </div>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control " name="cpf" id="id_cpf" value=""
+                                    autocomplete="off" data-digitar="cpf">
+                                <div class="form-control-feedback invalid-feedback" id="id_error_cpf"> </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </form>
             </div>
         </div>
     </div>
-    <!-- <div class="m-3">
-        <form action="#" method="post" id="formbtnnext">
-            <input type="hidden" name="id_form" id="id_form" value="4a">
-            <input type="hidden" name="idioma" id="idioma" value="<?php echo $lang ?>">
-            <button type="submit" disabled class="btn btn-primary" id='btn_next'>Proximo</button>
-        </form>
-    </div> -->
 </div>
--- theme/js/script.js
+++ theme/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
     var url_atual = window.location.href;
 	url_caminho = url_atual.length;
+
+    $('input[data-digitar="cpf"]').mask("999.999.999-99",{reverse:false});
+    $('input[data-digitar="cnpj"]').mask("99.999.999/9999-99",{reverse:false});
+    $('input[data-digitar="celular"]').mask("(99) 99999-9999",{reverse:false});
+    $('input[data-digitar="telefone"]').mask("(99) 9999-9999",{reverse:false});
+    $('input[data-digitar="cep"]').mask("99999-999",{reverse:false});
+
 });
