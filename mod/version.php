0bbb2e473e5b249e855f284addbe27896ae0e849
GitHub Action <action@github.com>
2026-08-26 23:15:55 +0000

    Versao atualizada automaticamente [skip ci]

--- mod/version.php
+++ mod/version.php
@@ -1,9 +1,9 @@
 <?php
 
 return array (
-  'version' => '0.1.121',
-  'build' => 121,
-  'commit' => '495ceeb',
+  'version' => '0.1.122',
+  'build' => 122,
+  'commit' => '27e848f',
   'branch' => 'HEAD',
-  'date' => '2026-08-26 19:36:11',
+  'date' => '2026-08-26 20:15:54',
 );
